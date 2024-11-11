513de759547ef5ba24feef9ff47185d2fec59b57
sudobaby <yhxyoo@proton.me>
2024-11-12 01:00:18 +0530

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import Exp from "@/components/exp";
 import Intro from "@/components/Intro";
@@ -9,6 +9,7 @@ import { HoverCard } from "@/components/ui/hover-card";
 import Xcard from "@/components/xcard";
 import { useLanyard } from "react-use-lanyard";
 import { ripples } from "ldrs"; // Import ldrs package
+import { Footer } from "@/components/layouts/footer"; // Import the Footer component
 
 // Register ripples for the preloader
 ripples.register();
@@ -30,14 +31,12 @@ export default function Home() {
   }, []);
 
   // Fallback image URL when nothing is playing
-  const fallbackImage = "/images/offlineplaying.png";
+  const fallbackImage = "https://media1.tenor.com/m/kBlYQKjd1rMAAAAd/edit.gif";
 
   return (
     <>
       {/* Main content */}
-      <div
-        className="w-7/12 mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
-      >
+      <div className="w-7/12 mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         {/* Image with no blur */}
         <Image
           layout="fill" // Makes the image take up the full container
@@ -67,9 +66,10 @@ export default function Home() {
             <Xcard />
             {data && (
               <div className="flex bg-neutral-800/5 text-zinc-300 items-center rounded-sm p-2 pt-2.5 mt-4">
+                {/* Avatar Image */}
                 <div className="h-16 w-16 relative rounded-lg overflow-hidden">
                   <img
-                    className="h-full w-full object-cover rounded-lg"
+                    className="h-full w-full object-cover rounded-lg" // Ensures square image with rounded corners
                     src={
                       data?.data.spotify?.album_art_url
                         ? data?.data.spotify?.album_art_url
@@ -95,6 +95,11 @@ export default function Home() {
           </main>
         )}
       </div>
+
+      {/* Footer */}
+      <footer className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-8/12 p-4 dark:bg-zinc-900">
+  <Footer />
+</footer>
     </>
   );
 }
